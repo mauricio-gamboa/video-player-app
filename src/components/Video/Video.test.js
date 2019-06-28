6201e54f572762ad9673387cbcc14826df36819a
Mauricio <magalocr@gmail.com>
2019-06-28 16:52:29 -0600

    Added tests to the video component.

--- src/components/Video/Video.test.js
+++ src/components/Video/Video.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render, fireEvent } from '@testing-library/react';
+
 import Video from './Video';
 
 describe('Video', () => {
@@ -7,5 +9,27 @@ describe('Video', () => {
 		const div = document.createElement('div');
 		ReactDOM.render(<Video />, div);
 		ReactDOM.unmountComponentAtNode(div);
+	});
+
+	describe('rendering the component', () => {
+		let props;
+
+		beforeEach(() => {
+			props = {
+				id: 'Jne9t8sHpUc',
+				videoId: 'Jne9t8sHpUc',
+				metaData: {
+					artist: 'Alanis',
+					title: 'Ironic'
+				}
+			};
+		});
+
+		it('renders the meta data', () => {
+			const text = `Playing ${props.metaData.title} by ${props.metaData.artist}`;
+			const { container } = render(<Video {...props} />);
+			const metaData = container.querySelector('.metaData');
+			expect(metaData.textContent).toBe(` ${text} `);
+		});
 	});
 });
